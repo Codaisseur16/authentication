c947d38a8e8bcebd30e75b3813d5fecb8f790e18
abbasalwash <abbas@alwash.nl>
2018-05-02 11:14:27 +0200

    added userchecker and userauth

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,39 @@
 import 'reflect-metadata'
-import {createKoaServer} from "routing-controllers"
+import { createKoaServer, Action, BadRequestError } from "routing-controllers"
+import { verify } from './jwt'
 
 const port = process.env.PORT || 4008
 
 const app = createKoaServer({
+  cors: true,
   controllers: [
-    //..
-  ]
+  ],
+  authorizationChecker: (action: Action) => {
+    const header: string = action.request.headers.authorization
+    if (header && header.startsWith('Bearer ')) {
+      const [ , token ] = header.split(' ')
+
+      try {
+        return !!(token && verify(token))
+      }
+      catch (e) {
+        throw new BadRequestError(e)
+      }
+    }
+    return false
+  },
+  currentUserChecker: async (action: Action) => {
+    const header: string = action.request.headers.authorization
+    if (header && header.startsWith('Bearer ')) {
+      const [ , token ] = header.split(' ')
+
+      if (token) {
+        const {id} = verify(token)
+        return {id}
+      }
+    }
+    return {}
+  }
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
